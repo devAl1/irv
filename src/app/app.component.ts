22d5889a8d5ce77ec1003f86a569f312986fc6fa
Alex Quintero <robquint@gmail.com>
2016-12-01 22:24:02 -0700

    sample things

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFire, FirebaseListObservable, FirebaseObjectObservable, AuthProviders, AuthMethods } from 'angularfire2';
+import {Subject} from 'rxjs/Subject';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,74 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  filteredItems: FirebaseListObservable<any[]>;
+  items: FirebaseListObservable<any[]>;
+  item: FirebaseObjectObservable<any>;
+  sizeSubject: Subject<any>
+
+  constructor(public af: AngularFire) {
+    this.items = af.database.list('/items');
+    this.item = af.database.object('/item');
+    
+    this.sizeSubject = new Subject();
+    this.filteredItems = af.database.list('/items', 
+    {
+      query: {
+        orderByChild: 'size',
+        equalTo: this.sizeSubject
+      }
+    });
+
+    this.item.set({something: 'cool', subobject: {morestuff: "blah blah"}});
+
+    this.af.auth.subscribe(auth => console.log(auth));
+  }
+
+  save(newName: string) {
+    this.item.set({ name: newName });
+  }
+  update(newSize: string) {
+    this.item.update({ size: newSize });
+  }
+  
+  delete() {
+    this.item.remove();
+  }
+
+  addItem(newName: string) {
+    this.items.push({ text: newName });
+  }
+  updateItem(key: string, newText: string) {
+    this.items.update(key, { text: newText });
+  }
+  updateItemSize(key: string, newSize: string){
+    this.items.update(key, { size: newSize });
+  }
+  deleteItem(key: string) {    
+    this.items.remove(key); 
+  }
+  deleteEverything() {
+    this.items.remove();
+  }
+
+  filterBy(size: string){
+    this.sizeSubject.next(size);
+  }
+
+  loginGitHub(){
+    this.af.auth.login({
+      provider: AuthProviders.Github,
+      method: AuthMethods.Popup,
+    });
+  }
+
+  loginGoogle(){
+     this.af.auth.login({
+      provider: AuthProviders.Google,
+      method: AuthMethods.Popup,
+    });
+  }
+
   title = 'app works!';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: "AIzaSyAhUJL0Rnmf_rbiaI8IuFYtIhJe4Ox3Q4o",
+  authDomain: "airv-a6f12.firebaseapp.com",
+  databaseURL: "https://airv-a6f12.firebaseio.com",
+  storageBucket: "airv-a6f12.appspot.com",
+  messagingSenderId: "451818901859"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +23,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule, 
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
